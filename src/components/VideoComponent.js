fee09279ab7ebffa32e3bbe04504463e73ae3410
olhabiek <olha.biekhtir@gmail.com>
2024-07-30 23:34:16 +0200

    added video

--- src/components/VideoComponent.js
+++ src/components/VideoComponent.js
@@ -1,4 +1,16 @@
 function VideoComponent() {
-  return <video src="https://www.youtube.com/watch?v=-452p_9ESbM" />;
+  return (
+    <iframe
+      width="600"
+      height="480"
+      src="https://www.youtube.com/embed/-452p_9ESbM"
+      title="Котики и  милые котята ))) Милое видео"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+    ></iframe>
+  );
 }
+
 export default VideoComponent;
